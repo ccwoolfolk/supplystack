1916986f6f92e4d721ffdc875a22d76dbf717e80
Casey Woolfolk <ccwoolfolk@gmail.com>
2016-07-30 23:16:10 -0500

    Add tests for supply stack addition

--- tests/testthat/test_supplystack_base.R
+++ tests/testthat/test_supplystack_base.R
@@ -17,3 +17,42 @@ test_that("Components are sorted", {
 test_that("Returns supply stack object", {
   expect_is(supplystack(p=1:5, q=rep(10, 5)), "supplystack")
 })
+
+test_that("Adding stacks together results in a combined stack", {
+  prices <- runif(min=100, max=200, n=10)
+  quantities <- rep(1:5, 2)
+  producer_names <- paste("Producer", 1:10, sep="")
+  
+  ss_1 <- supplystack(p=prices[1:5],
+                      q=quantities[1:5],
+                      nms=producer_names[1:5])
+  ss_2 <- supplystack(p=prices[6:10],
+                      q=quantities[6:10],
+                      nms=producer_names[6:10])
+  ss_3 <- supplystack(p=prices[6:10],
+                      q=quantities[6:10])
+  ss_comb <- supplystack(p=prices,
+                         q=quantities,
+                         nms=producer_names)
+  
+  expect_equal(ss_1 + ss_2, ss_comb)
+  expect_equal(ss_2 + ss_1, ss_comb)
+  
+})
+
+test_that("Adding stack with names to one without results in no names", {
+  prices <- runif(min=100, max=200, n=10)
+  quantities <- rep(1:5, 2)
+  producer_names <- paste("Producer", 1:10, sep="")
+  
+  ss_1 <- supplystack(p=prices[1:5],
+                      q=quantities[1:5],
+                      nms=producer_names[1:5])
+  ss_2 <- supplystack(p=prices[6:10],
+                      q=quantities[6:10])
+  ss_comb <- supplystack(p=prices,
+                         q=quantities)
+  
+  expect_equal(ss_1 + ss_2, ss_comb)
+  
+})
